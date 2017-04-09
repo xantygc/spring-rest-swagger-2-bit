9c154bf68d7a72073e630e5e5fcb63c6597b8cf2
santy <santiago.gonzalez.courel@gmail.com>
2017-04-09 12:54:53 +0200

    [REST-SWAGGER-2-BIT v1.0.0] Open version

---
+++ src/main/java/com/axpo/bit/SpringRestSwagger2BitApplication.java
@@ -0,0 +1,12 @@
+package com.axpo.bit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSwagger2BitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSwagger2BitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/axpo/bit/SpringRestSwagger2BitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.axpo.bit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestSwagger2BitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
